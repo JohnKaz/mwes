58cb62042d8c849f045326dee3cd733fd15bea79
Yannis Kazos <kazosj@gmail.com>
2025-01-26 12:44:19 +0200

    Create move_mwes.py

---
+++ move_mwes.py
@@ -0,0 +1,59 @@
+from conllu import parse_incr
+
+def move_mwes(input_file, output_file, field):
+
+    # store mwe type info
+    mwe_type = [''] * 8
+    
+    for sentence in parse_incr(input_file):
+        
+        # first pass: get mwe types
+        for token in sentence:
+            if field == "xpos":
+                token["xpos"] = "_"
+            
+            misc = token["misc"]
+            
+            # check if misc contains mwe information
+            if misc != None and "mwe" in misc:
+
+                # check if it contains mwe category information
+                if misc["mwe"] != None and len(misc["mwe"]) > 1 and misc["mwe"][1] != ';':
+
+                    # store mwe type in appropriate slot
+                    if misc["mwe"][0].isdigit():
+                        i = int(misc["mwe"][0])
+                        misc["mwe"] = misc["mwe"].replace(' ', '')
+                        parts = misc["mwe"].split(':')
+                        if len(parts) > 1:
+                            mwe_type[i] = ":" + parts[1].lower().replace('.', '')
+                    else:
+                        print(sentence.metadata["sent_id"])
+                    continue
+
+        # second pass: edit all fields (deprel or XPOS)
+        if field == "deprel":
+            for token in sentence:
+                
+                misc = token["misc"]
+                # if token is marked as mwe, edit its deprel tag appropriately
+                if misc != None and "mwe" in misc and misc["mwe"] != None:
+                    
+                    i = int(misc["mwe"][0])
+                    token["deprel"] = token["deprel"] + mwe_type[i]
+        elif field == "xpos":
+            for token in sentence:
+                
+                misc = token["misc"]
+                # if token is marked as mwe, edit its XPOS tag appropriately
+                if misc != None and "mwe" in misc and misc["mwe"] != None:
+                    
+                    i = int(misc["mwe"][0])
+                    token["xpos"] = mwe_type[i].upper()[1:]
+        else:
+            print("Error: Incorrect Field")
+
+        # append altered conllu sentence to desired output file
+        with open(output_file, "a") as f:
+            serialized = sentence.serialize()
+            f.write(serialized)
