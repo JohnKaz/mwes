6e08de9632963e41a531a6af2facbbfaa6c82173
Yannis Kazos <kazosj@gmail.com>
2025-02-04 18:21:08 +0200

    Update move_mwes.py

--- move_mwes.py
+++ move_mwes.py
@@ -1,59 +1,97 @@
 from conllu import parse_incr
 
-def move_mwes(input_file, output_file, field):
+def move_mwes(input_file, output_file, field_in, field_out, keep=False):
 
-    # store mwe type info
-    mwe_type = [''] * 8
-    
-    for sentence in parse_incr(input_file):
+    # case one: Move mwe annotations from "deprel" field to "xpos"
+    if field_in == "deprel" and field_out == "xpos":
+        for sentence in parse_incr(input_file):
+            for token in sentence:
+                temp = token["deprel"].split(":")[-1]
+                if temp in ["vid", "lvc", "lvcfull", "lvccause", "vpc", "vpcfull", "mvc", "iav", "irv"]:
+                    token["xpos"] = temp.upper()
+                    # delete mwe info from "deprel" unless otherwise specified
+                    if keep == False:
+                        token["deprel"] = token["deprel"].rsplit(":", 1)[0]
+                else:
+                    token["xpos"] = "_"
+
+            # append modified conllu sentence to desired output file
+            with open(output_file, "a") as f:
+                serialized = sentence.serialize()
+                f.write(serialized)
+                
+    # case two: Move mwe annotations from "xpos" field to "deprel"
+    elif field_in == "xpos" and field_out == "deprel":
+        for sentence in parse_incr(input_file):
+            for token in sentence:
+                if token["xpos"] != None:
+                    temp = token["xpos"].lower()
+                    if temp in ["vid", "lvc", "lvcfull", "lvccause", "vpc", "vpcfull", "mvc", "iav", "irv"]:
+                        token["deprel"] = token["deprel"] + ":" + temp
+                        # delete mwe info from "xpos" unless otherwise specified
+                        if keep == False:
+                            token["xpos"] = "_"
+                            
+            # append modified conllu sentence to desired output file
+            with open(output_file, "a") as f:
+                serialized = sentence.serialize()
+                f.write(serialized)
+                
+    elif field_in == "misc":
+        # store mwe type info
+        mwe_type = [''] * 8
         
-        # first pass: get mwe types
-        for token in sentence:
-            if field == "xpos":
-                token["xpos"] = "_"
+        for sentence in parse_incr(input_file):
             
-            misc = token["misc"]
-            
-            # check if misc contains mwe information
-            if misc != None and "mwe" in misc:
-
-                # check if it contains mwe category information
-                if misc["mwe"] != None and len(misc["mwe"]) > 1 and misc["mwe"][1] != ';':
-
-                    # store mwe type in appropriate slot
-                    if misc["mwe"][0].isdigit():
+            # first pass: get mwe types
+            for token in sentence:
+                if field_out == "xpos":
+                    token["xpos"] = "_"
+                
+                misc = token["misc"]
+                
+                # check if misc contains mwe information
+                if misc != None and "mwe" in misc:
+    
+                    # check if it contains mwe category information
+                    if misc["mwe"] != None and len(misc["mwe"]) > 1 and misc["mwe"][1] != ';':
+    
+                        # store mwe type in appropriate slot
+                        if misc["mwe"][0].isdigit():
+                            i = int(misc["mwe"][0])
+                            misc["mwe"] = misc["mwe"].replace(' ', '')
+                            parts = misc["mwe"].split(':')
+                            if len(parts) > 1:
+                                mwe_type[i] = ":" + parts[1].lower().replace('.', '')
+                        else:
+                            print(sentence.metadata["sent_id"])
+                        continue
+    
+            # second pass: edit all fields (deprel or XPOS)
+            if field_out == "deprel":
+                for token in sentence:
+                    
+                    misc = token["misc"]
+                    # if token is marked as mwe, edit its deprel tag appropriately
+                    if misc != None and "mwe" in misc and misc["mwe"] != None:
+                        
                         i = int(misc["mwe"][0])
-                        misc["mwe"] = misc["mwe"].replace(' ', '')
-                        parts = misc["mwe"].split(':')
-                        if len(parts) > 1:
-                            mwe_type[i] = ":" + parts[1].lower().replace('.', '')
-                    else:
-                        print(sentence.metadata["sent_id"])
-                    continue
-
-        # second pass: edit all fields (deprel or XPOS)
-        if field == "deprel":
-            for token in sentence:
-                
-                misc = token["misc"]
-                # if token is marked as mwe, edit its deprel tag appropriately
-                if misc != None and "mwe" in misc and misc["mwe"] != None:
+                        token["deprel"] = token["deprel"] + mwe_type[i]
+            elif field_out == "xpos":
+                for token in sentence:
                     
-                    i = int(misc["mwe"][0])
-                    token["deprel"] = token["deprel"] + mwe_type[i]
-        elif field == "xpos":
-            for token in sentence:
-                
-                misc = token["misc"]
-                # if token is marked as mwe, edit its XPOS tag appropriately
-                if misc != None and "mwe" in misc and misc["mwe"] != None:
-                    
-                    i = int(misc["mwe"][0])
-                    token["xpos"] = mwe_type[i].upper()[1:]
-        else:
-            print("Error: Incorrect Field")
-
-        # append altered conllu sentence to desired output file
-        with open(output_file, "a") as f:
-            serialized = sentence.serialize()
-            f.write(serialized)
+                    misc = token["misc"]
+                    # if token is marked as mwe, edit its XPOS tag appropriately
+                    if misc != None and "mwe" in misc and misc["mwe"] != None:
+                        
+                        i = int(misc["mwe"][0])
+                        token["xpos"] = mwe_type[i].upper()[1:]
+            else:
+                print("Error: Unexpected field input")
+        
+            # append altered conllu sentence to desired output file
+            with open(output_file, "a") as f:
+                serialized = sentence.serialize()
+                f.write(serialized)
+    else:
+        print("Error: Unexpected field input")
